7ed451e6eee0b204c2552513c586e90adfd353ca
saikatbsk <saikatbsk@gmail.com>
2015-05-31 08:28:02 +0530

    tabstop changed to 4

--- DataStructure/C1_heapify/heapify.c
+++ DataStructure/C1_heapify/heapify.c
@@ -1,48 +1,48 @@
 #include "heapify.h"
 
 void buildMaxHeap(int *Arr, int n) {
-  int i;
+	int i;
 
-  /* call maxHeapify recursively on every non-leaf node */
-  for(i = n/2 ; i >= 0 ; i--)
-    maxHeapify(Arr, i, n);
+	/* call maxHeapify recursively on every non-leaf node */
+	for(i = n/2 ; i >= 0 ; i--)
+		maxHeapify(Arr, i, n);
 
-  return;
+	return;
 }
 
 void maxHeapify(int* Arr, int i, int n) {
-  int leftChild, rightChild, max;
+	int leftChild, rightChild, max;
 
-  /* these two variables hold the indexes of the left and the right child */
-  leftChild = 2*i + 1;
-  rightChild = 2*i + 2;
+	/* these two variables hold the indexes of the left and the right child */
+	leftChild = 2*i + 1;
+	rightChild = 2*i + 2;
 
-  /* the max variable holds the index of the parent */
-  max = i;
+	/* the max variable holds the index of the parent */
+	max = i;
 
-  /* if the left child is greater than the parent set max = leftChild*/
-  if(leftChild < n && Arr[leftChild] > Arr[max])
-    max = leftChild;
+	/* if the left child is greater than the parent set max = leftChild*/
+	if(leftChild < n && Arr[leftChild] > Arr[max])
+		max = leftChild;
 
-  /* if the right child is greater than the parent set max = rightChild */
-  if(rightChild < n && Arr[rightChild] > Arr[max])
-    max = rightChild;
+	/* if the right child is greater than the parent set max = rightChild */
+	if(rightChild < n && Arr[rightChild] > Arr[max])
+		max = rightChild;
 
-  /* if max is not the parent swap the parent with the largest value */
-  if(max != i) {
-    swap(&Arr[i], &Arr[max]);
+	/* if max is not the parent swap the parent with the largest value */
+	if(max != i) {
+		swap(&Arr[i], &Arr[max]);
 
-    /* call maxHeapify on the child with the new value */
-    maxHeapify(Arr, max, n);
-  }
+		/* call maxHeapify on the child with the new value */
+		maxHeapify(Arr, max, n);
+	}
 
-  return;
+	return;
 }
 
 void swap(int* a, int* b) {
-  *a = *a+*b;
-  *b = *a-*b;
-  *a = *a-*b;
+	*a = *a+*b;
+	*b = *a-*b;
+	*a = *a-*b;
 
-  return;
+	return;
 }
